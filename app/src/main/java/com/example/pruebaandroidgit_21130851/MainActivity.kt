560ec3093c64e4b2b8b8f62953df3ed273813617
sailent2k <dylanarturoh@outlook.es>
2024-09-06 13:30:59 -0600

    Proyecto inicial

---
+++ app/src/main/java/com/example/pruebaandroidgit_21130851/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.pruebaandroidgit_21130851
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pruebaandroidgit_21130851.ui.theme.PruebaAndroidGIT_21130851Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PruebaAndroidGIT_21130851Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PruebaAndroidGIT_21130851Theme {
+        Greeting("Android")
+    }
+}
